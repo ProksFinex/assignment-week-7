2ec6e4b45c45d5bdabcfe97511e56bfb82f6888b
ProksFinex <prabhatjnu2@gmail.com>
2024-10-06 00:00:41 +0530

    fontend for paragenerator

---
+++ paraGenerator/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Paragraph Generator</h1>
+
+      <input type='number' placeholder='enter Number of Words'></input>
+      <button className='button'>Go</button>
+
+    </>
+  )
+}
+
+export default App
