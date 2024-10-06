650cb1729e72e9c0da98610a0507fb22d1e0eb4c
ProksFinex <prabhatjnu2@gmail.com>
2024-10-06 23:39:47 +0530

    first update completed application

--- paraGenerator/src/App.jsx
+++ paraGenerator/src/App.jsx
@@ -1,18 +1,43 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { data } from "./constant";
 
 function App() {
-  
+  const [count, setCount] = useState(0);
+  const [text,setText]=useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if(amount<0) amount=1;
+    if(amount>5) amount=5;
+    setText(data.slice(0,amount))
+    return;
+  };
 
   return (
     <>
       <h1>Paragraph Generator</h1>
 
-      <input type='number' placeholder='enter Number of Words'></input>
-      <button className='button'>Go</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          placeholder="enter Number of Words"
+          value={count}
+          onChange={(e) => setCount(
+            e.target.value)
+          }
+        />
+        <button className="button">Go</button>
+      </form>
 
+      <article className="para">
+        {text.map((data,index)=>(
+          <p key={index}>{data}</p>
+        ))}
+      </article>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
